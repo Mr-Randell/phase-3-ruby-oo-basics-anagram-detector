47373147fd3f7e62983cd6a96cfa43b70009cfe3
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-10-25 20:18:43 +0300

    commit tree 1fa49b901be201e7a3cdc558a667f358cc6ea464

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(string)
+        string.filter do |string_letters|
+            string_letters.downcase.split("").sort == word.downcase.split("").sort
+        end
+    end
+end
